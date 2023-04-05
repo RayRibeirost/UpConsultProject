d6e3292f991ee28f69d105ed499adb44c58608db
khadidjamoraes <khadidja.developer@gmail.com>
2023-04-05 09:13:27 -0300

    Create cadastroConsultor.js

---
+++ backend/models/cadastroConsultor.js
@@ -0,0 +1,59 @@
+const seq = require('sequelize');
+const db2 = require('./db2');
+
+const Cadastro_Consultor = db2.define('Cadastro_Consultor',
+{
+    Nome:
+    {
+        type: seq.STRING,
+        allowNull: false
+    },
+    CNPJ:
+    {
+        type: seq.INTEGER,
+        primaryKey: true,
+        allowNull: false
+    },
+    Email:
+    {
+        type: seq.STRING,
+        allowNull: false
+    },
+    Senha:
+    {
+        type: seq.STRING,
+        allowNull: false
+    },
+    Confsenha:
+    {
+        type: seq.STRING,
+        allowNull: false
+    },
+    Endereco:
+    {
+        type: seq.STRING,
+        allowNull: false
+    },
+    DataNasc: {
+        type: seq.DATE,
+        allowNull: true
+    },
+    Sexo:
+    {
+        type: seq.CHAR,
+        allowNull: false
+    },
+    Formacao:
+    {
+        type: seq.STRING,
+        allowNull: false
+    },
+    Anexo_Docs:
+    {
+        type: seq.BLOB,
+        allowNull: false
+    }
+});
+
+//cadastroConsultor.sync();
+module.exports = Cadastro_Consultor;
