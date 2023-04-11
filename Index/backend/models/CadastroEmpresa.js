a56aa7d76c7fb0e6d95998dfa0fe087e8c2d321a
khadidjamoraes <khadidja.developer@gmail.com>
2023-04-11 09:25:50 -0300

    Update CadastroEmpresa.js

--- Index/backend/models/CadastroEmpresa.js
+++ Index/backend/models/CadastroEmpresa.js
@@ -1,4 +1,4 @@
-const db2 = require('./db2');
+/*const db2 = require('./db2');
 const seq = require ('sequelize');
 
 const CadastroEmpresa = db2.define('Cadastro_Empresa', {
@@ -39,4 +39,26 @@ const CadastroEmpresa = db2.define('Cadastro_Empresa', {
 })
     
 // CadastroEmpresa.sync();
-module.exports = CadastroEmpresa;
+module.exports = CadastroEmpresa; 
+
+
+app.post("/cadastrarEmpresa", async (req, res) => {
+    //console.log(req.body);
+
+    await CadastroEmpresa.create({
+        Nome_Empresa: req.body.nome, 
+        CNPJ: req.body.cnpj, 
+        Email: req.body.email, 
+        Senha: req.body.senha,
+        Confsenha: req.body.confsenha
+    })
+    .then(() => {
+        return res.status(200).sendFile('C:/Users/claud/OneDrive/Área de Trabalho/JGT codes/UpConsultProject/Index/confirmacao-empresa.html');
+    }).catch(() => {
+        return res.status(400).json({
+            erro: true,
+            mensagem: "Erro: Usuário não cadastrado com sucesso!"
+        });
+    });
+    //res.send("Página cadastrar");
+});*/
